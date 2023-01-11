b9536df8f467516b6095eb30030424fc34f65e92
gozdemogus <gozdemog@gmail.com>
2023-01-11 15:48:24 +0300

    connection count added

--- UpSchool_SignalR_Api/Hubs/MyHub.cs
+++ UpSchool_SignalR_Api/Hubs/MyHub.cs
@@ -9,14 +9,31 @@ namespace UpSchool_SignalR_Api.Hubs
     public class MyHub : Hub
     {
         public static List<string> Names { get; set; } = new List<string>();
+
+        public static int ClientCount { get; set; } = 0;
+
         public async Task SendName(string name)
         {
             Names.Add(name);
             await Clients.All.SendAsync("ReceiveName", name);
         }
+
         public async Task GetNames()
         {
             await Clients.All.SendAsync("ReceiveNames", Names);
         }
+
+        //baglanınca 1 artar
+        public async override Task OnConnectedAsync()
+        {
+            ClientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+        }
+
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            ClientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+        }  
     }
 }
